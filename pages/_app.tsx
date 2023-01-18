2cdc2eedcfc0d0f2e179896ba2b8438bbbdbb1cb
fe.hyunsu <fe.hyunsu@gmail.com>
2023-01-18 23:54:43 +0900

    - feature : recoil update.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../styles/global-style";
 import { theme } from "../styles/theme";
@@ -9,22 +10,24 @@ import Footer from "../components/layout/footer/Footer";
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="Project title" />
-        <meta property="og:description" content="Project description" />
-        <meta property="og:image" content="" />
-        <link rel="icon" href="/favicon.ico" />
-        <title>Project Name</title>
-      </Head>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Header />
-        <main>
-          <Component {...pageProps} />
-        </main>
-        <Footer />
-      </ThemeProvider>
+      <RecoilRoot>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="og:title" content="Project title" />
+          <meta property="og:description" content="Project description" />
+          <meta property="og:image" content="" />
+          <link rel="icon" href="/favicon.ico" />
+          <title>Project Name</title>
+        </Head>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <Header />
+          <main>
+            <Component {...pageProps} />
+          </main>
+          <Footer />
+        </ThemeProvider>
+      </RecoilRoot>
     </>
   );
 };

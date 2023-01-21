3ad27d74567041cff63b824e1196e65beb97aa53
fe.hyunsu <fe.hyunsu@gmail.com>
2023-01-21 22:53:21 +0900

    feature : list page update.

--- pages/order.tsx
+++ pages/order.tsx
@@ -3,7 +3,16 @@ import React from "react";
 const Order = () => {
   return (
     <>
-      <div>order</div>
+      <div>
+        <ul>
+          <li>2023.01.23</li>
+          <li>2023.01.23</li>
+          <li>2023.01.23</li>
+          <li>2023.01.23</li>
+          <li>2023.01.23</li>
+          <li>메인, 일자 선택, 주문, 만들기</li>
+        </ul>
+      </div>
     </>
   );
 };

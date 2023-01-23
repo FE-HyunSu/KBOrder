137bf0e269e1d95b21489709166c85c69595f5b9
fe.hyunsu <fe.hyunsu@gmail.com>
2023-01-23 22:24:08 +0900

    - feature : useRef.

--- components/intro/Intro.tsx
+++ components/intro/Intro.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { ElementRef, useRef } from "react";
 import { IntroUI } from "./IntroStyle";
 import Image from "next/image";
 import ImgLogo from "../../public/images/img_logo.png";
 
 const Intro = () => {
+  const nameRef: any = useRef();
   return (
     <>
       <IntroUI>
@@ -17,7 +18,7 @@ const Intro = () => {
         <p>
           <span>오늘 김밥 드실분?</span>
         </p>
-        <input type="text" placeholder="이름을 입력해 주세요" />
+        <input type="text" placeholder="이름을 입력해 주세요" ref={nameRef} />
         <button type="button">주문하기</button>
       </IntroUI>
     </>

761157c8e267409627bf74513ea48763cd6bf743
fe.hyunsu <fe.hyunsu@gmail.com>
2023-02-20 12:00:35 +0900

    feat : order list firebase data.

--- components/orderlist/OrderList.tsx
+++ components/orderlist/OrderList.tsx
@@ -1,33 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { OrderListUI, BtnCreateOrder } from "./OrderListStyle";
-
-interface listType {
-  id: number;
-  title: string;
-  code: string;
-}
-
-const list = [
-  { id: 0, title: "2023년2월8일", code: "p1" },
-  { id: 1, title: "2023년2월7일", code: "p2" },
-  { id: 2, title: "2023년2월6일", code: "p3" },
-  { id: 3, title: "2023년2월5일", code: "p4" },
-  { id: 4, title: "2023년2월4일", code: "p5" },
-];
+import { getData } from "../../api/firestore";
 
 const OrderList = () => {
+  const [isList, setList] = useState([]);
+  const getList = async () => {
+    await getData("order").then((data) => {
+      let orderList: any = {};
+      orderList = data.docs.map((item: any) => {
+        return { ...item.data() };
+      });
+      setList(orderList);
+    });
+  };
+  useEffect(() => {
+    getList();
+  }, []);
   return (
     <>
       <OrderListUI>
         <div className="inner">
           <h1>주문 목록</h1>
           <ul>
-            {list &&
-              list.map((item: listType) => {
+            {isList &&
+              isList.map((item: any, idx: any) => {
                 return (
-                  <li key={item.id}>
-                    <Link href={`/list/` + item.code}>{item.title}</Link>
+                  <li key={idx}>
+                    <Link href={`/list/` + item.date}>{item.date}</Link>
                   </li>
                 );
               })}

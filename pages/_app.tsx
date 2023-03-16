96991b1cdc524100d5bef554bf12dc402354ed10
fe.hyunsu <fe.hyunsu@gmail.com>
2023-03-16 21:15:38 +0900

    feature : update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
+import React, { useState } from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../styles/global-style";
@@ -12,30 +14,45 @@ import "dayjs/locale/ko"; // 한국어 가져오기.
 dayjs.extend(isLeapYear); // 플러그인 등록.
 dayjs.locale("ko"); // 언어 등록.
 
+const [client] = useState(
+  () =>
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          // 데이터가 stale 상태일 경우 윈도우 포커싱 될 때 마다 refetch를 실행하는 옵션. (boolean | "always")
+          refetchOnWindowFocus: false,
+          staleTime: 60 * 1000, // 1분.
+        },
+      },
+    })
+);
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <RecoilRoot>
-        <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
-          />
-          <meta property="og:title" content="KBOrder" />
-          <meta property="og:description" content="KBOrder" />
-          <meta property="og:image" content="" />
-          <link rel="icon" href="/favicon.ico" />
-          <title>🍙 KBOrder</title>
-        </Head>
-        <GlobalStyle />
-        <ThemeProvider theme={theme}>
-          <Header />
-          <main>
-            <Component {...pageProps} />
-          </main>
-          <Footer />
-        </ThemeProvider>
-      </RecoilRoot>
+      <QueryClientProvider client={client}>
+        <RecoilRoot>
+          <Head>
+            <meta
+              name="viewport"
+              content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
+            />
+            <meta property="og:title" content="KBOrder" />
+            <meta property="og:description" content="KBOrder" />
+            <meta property="og:image" content="" />
+            <link rel="icon" href="/favicon.ico" />
+            <title>🍙 KBOrder</title>
+          </Head>
+          <GlobalStyle />
+          <ThemeProvider theme={theme}>
+            <Header />
+            <main>
+              <Component {...pageProps} />
+            </main>
+            <Footer />
+          </ThemeProvider>
+        </RecoilRoot>
+      </QueryClientProvider>
     </>
   );
 };

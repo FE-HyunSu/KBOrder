13ceeca12b67527df74fd69b6ae7ab3904f756d5
fe.hyunsu <fe.hyunsu@gmail.com>
2023-03-22 16:01:38 +0900

    feat : build test.

--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import OrderList from "@components/Order/List";
+// import OrderList from "@components/Order/List";
 
 const List = () => {
-  return (
-    <>
-      <OrderList />
-    </>
-  );
+  return <>{/* <OrderList /> */} build test</>;
 };
 
 export default List;

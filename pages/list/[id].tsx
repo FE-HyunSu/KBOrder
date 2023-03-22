589c2164f29cba02d9c27a5be1f0b65aeed84e46
fe.hyunsu <fe.hyunsu@gmail.com>
2023-03-22 15:45:02 +0900

    fix : issue check.

--- pages/list/[id].tsx
+++ pages/list/[id].tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import OrderDetail from "@components/Order/Detail";
+// import OrderDetail from "@components/Order/Detail";
+import OrderDetail from "../../src/@views/Order/Detail";
 
 const ListDetail = () => {
   return (

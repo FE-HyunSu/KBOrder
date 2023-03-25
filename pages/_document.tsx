f67bd0cba7f04debafb3354511072080d174321a
fe.hyunsu <fe.hyunsu@gmail.com>
2023-03-25 15:04:30 +0900

    feat : meta info update.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,12 +39,15 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <link rel="shortcut icon" href="/favicon.ico" type="image/svg" />
-          <meta property="og:title" content="" />
-          <meta property="og:image" content="" />
-          <meta property="og:description" content="" />
-          <meta property="og:url" content="/" />
-          <meta name="description" content="" />
-          <meta name="keywords" content="" />
+          <meta property="og:title" content="KB-Order" />
+          <meta property="og:image" content="/images/img_meta.png" />
+          <meta
+            property="og:description"
+            content="삼성동 이레 김밥주문 페이지"
+          />
+          <meta property="og:url" content="https://kb-order.vercel.app" />
+          <meta name="description" content="삼성동 이레 김밥주문 페이지" />
+          <meta name="keywords" content="김밥주문" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&display=swap"

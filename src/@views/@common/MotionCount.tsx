3fa8909a412847eec8e152ae3bf46f66ec52034a
fe.hyunsu <fe.hyunsu@gmail.com>
2023-05-17 09:09:43 +0900

    feat : countMotion update.

--- src/@views/@common/MotionCount.tsx
+++ src/@views/@common/MotionCount.tsx
@@ -7,26 +7,28 @@ interface CountType {
 
 const MotionCount = ({ count, sec }: CountType) => {
   const countRef = useRef<HTMLElement>(null);
+  const timeSec = !!sec ? sec : 2000;
   const unitWon = (num: number) => {
     return Math.round(num)
       .toString()
       .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   };
 
-  useEffect(() => {
-    let resultNumber = 0;
-    const requestCount = () => {
-      try {
-        if (resultNumber >= count && countRef.current) {
-          countRef.current.innerHTML = unitWon(count);
-        } else if (countRef.current) {
-          countRef.current.innerHTML = unitWon(Math.round((resultNumber += count / ((!!sec ? sec : 2000) / 60))));
-          requestAnimationFrame(requestCount);
-        }
-      } catch (e) {
-        console.log(e);
+  let resultNumber = 0;
+  const requestCount = () => {
+    try {
+      if (resultNumber >= count && countRef.current) {
+        countRef.current.innerHTML = unitWon(count);
+      } else if (countRef.current) {
+        countRef.current.innerHTML = unitWon(Math.round((resultNumber += count / ((timeSec / 1000) * 60))));
+        requestAnimationFrame(requestCount);
       }
-    };
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
     requestAnimationFrame(requestCount);
   }, []);
   return <i ref={countRef}></i>;

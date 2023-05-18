1eb34a9763a200b3ca6815325cbd2f692cdb0645
fe.hyunsu <fe.hyunsu@gmail.com>
2023-05-18 09:15:34 +0900

    feat : MotionCount 컴포넌트 수정. customHooks 추가.

--- src/@views/@common/MotionCount.tsx
+++ src/@views/@common/MotionCount.tsx
@@ -5,26 +5,23 @@ interface CountType {
   sec?: number;
 }
 
+const unitWon = (num: number) => {
+  return Math.round(num)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
 const MotionCount = ({ count, sec }: CountType) => {
   const countRef = useRef<HTMLElement>(null);
   const timeSec = !!sec ? sec : 2000;
-  const unitWon = (num: number) => {
-    return Math.round(num)
-      .toString()
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  };
 
   let resultNumber = 0;
   const requestCount = () => {
-    try {
-      if (resultNumber >= count && countRef.current) {
-        countRef.current.innerHTML = unitWon(count);
-      } else if (countRef.current) {
-        countRef.current.innerHTML = unitWon(Math.round((resultNumber += count / ((timeSec / 1000) * 60))));
-        requestAnimationFrame(requestCount);
-      }
-    } catch (e) {
-      console.log(e);
+    if (resultNumber >= count && countRef.current) {
+      countRef.current.innerText = unitWon(count);
+    } else if (countRef.current) {
+      countRef.current.innerText = unitWon(Math.round((resultNumber += count / ((timeSec / 1000) * 60))));
+      requestAnimationFrame(requestCount);
     }
   };
 
@@ -33,4 +30,5 @@ const MotionCount = ({ count, sec }: CountType) => {
   }, []);
   return <i ref={countRef}></i>;
 };
+
 export default MotionCount;
---
+++ src/hooks/useMotionCount.tsx
@@ -0,0 +1,38 @@
+import React, { useRef, useEffect } from 'react';
+
+interface CountType {
+  count: number;
+  motionTime?: number;
+}
+
+const unitWon = (num: number) => {
+  return Math.round(num)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+const useMotionCount = ({ count, motionTime }: CountType) => {
+  const countRef = useRef<HTMLElement>(null);
+  const timeSec = !!motionTime ? motionTime : 2000; // 초기값 설정.
+  let requestCount: () => void;
+
+  useEffect(() => {
+    let resultNumber = 0;
+    requestCount = () => {
+      if (resultNumber >= count && countRef.current) {
+        countRef.current.innerText = unitWon(count);
+      } else if (countRef.current) {
+        countRef.current.innerText = unitWon(Math.round((resultNumber += count / ((timeSec / 1000) * 60))));
+        requestAnimationFrame(requestCount);
+      }
+    };
+    let rafRequestCount = requestAnimationFrame(requestCount);
+    return () => {
+      cancelAnimationFrame(rafRequestCount);
+    };
+  }, []);
+
+  return { countRef };
+};
+
+export default useMotionCount;
